8ed493de20d55352699378b2d3ce63c30df0e2b5
7204-OMER <Eyupbhr24@gmail.com>
2021-06-07 18:45:38 +0300

    add buttons and form control

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -18,7 +18,21 @@ export const Signup = () => {
           <h1 className="my-4 font-wight-bold-display-4">Sign Up</h1>
           <Form>
             <TextField label="First Name" name="firstName" type="text" />
-            <TextField label="First Name" name="firstName" type="text" />
+            <TextField label="Last Name" name="LastName" type="text" />
+            <TextField label="Email" name="Email" type="email" />
+            <TextField label="Password" name="Password" type="password" />
+            <TextField
+              label="Confirm Password"
+              name="ConfirmPassword"
+              type="password"
+            />
+            <TextField
+              label="I accept terms & conditions"
+              name="aggreeToTerms"
+              type="ckeckbox"
+            />
+            <button className="btn btn-dark mt-3">Register</button>
+            <button className="btn btn-danger mt-3 ml-3">Reset</button>
           </Form>
         </div>
       )}
--- src/components/TextField.js
+++ src/components/TextField.js
@@ -3,11 +3,16 @@ import { useField } from "formik";
 
 export const TextField = ({ label, ...props }) => {
   const [field, meta] = useField(props);
-  console.log({ field, meta });
   return (
     <div className="mb-2">
       <label htmlFor={field.name}>{label}</label>
-      <input id={field.name} />
+      <input
+        className={`form-control shadow-none`}
+        id={field.name}
+        {...field}
+        {...props}
+        autocomplete="off"
+      />
     </div>
   );
 };
